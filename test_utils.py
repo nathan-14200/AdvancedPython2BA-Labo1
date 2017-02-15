09407691f08ab53c8fb24140f8577af3d265e339
nathan-14200 <nathan_3145@hotmail.com>
2017-02-15 14:48:51 +0100

    Add some tests

--- test_utils.py
+++ test_utils.py
@@ -8,16 +8,20 @@ import utils
 class TestUtils(unittest.TestCase):
     def test_fact(self):
         # À compléter...
+        self.assertEquals(utils.fact(2), 4)
         self.assertEquals(utils.fact(0), 1)
+        self.assertEquals(utils.fact(-3), None)
         pass
     
     def test_roots(self):
         # À compléter...
-        self.assertEquals(utils.roots(0, 0, 0), 0)
+        self.assertEquals(utils.roots(1, 1, 1), ())
+        self.assertEquals(utils.roots(1, 4, 4), (-2.0,))
         pass
     
     def test_integrate(self):
-        self.assertEquals(utils.integrate('1', 0, 1), '1')
+        self.assertEquals(utils.integrate('1', 0, 1), 1.0)
+        self.assertEquals(utils.integrate('0', 0, 0), 0.0)
         # À compléter...
         pass
 
